e5a07505182a3c1dbad777b62b059c947e5fef42
708hirasawa <70517152+708hirasawa@users.noreply.github.com>
2020-11-13 16:13:06 +0900

    Update manage_edit_sakagura.php
    創業年に2020年令和2年を追加

--- manage_edit_sakagura.php
+++ manage_edit_sakagura.php
@@ -219,6 +219,7 @@ function writeSakaguraContainer()
 			  print('<div class="column2">');
 				print('<SELECT name="establishment">');
 				  print('<OPTION VALUE="">創業・設立年の選択</OPTION>');
+				  print('<OPTION VALUE=2020>2020年 (令和2年)</OPTION>');
 				  print('<OPTION VALUE=2019>2019年 (令和1年)</OPTION>');
 				  print('<OPTION VALUE=2018>2018年 (平成30年)</OPTION>');
 				  print('<OPTION VALUE=2017>2017年 (平成29年)</OPTION>');
